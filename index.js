68fc3019a295e7809b2ee3e87083371ebcd39799
liuguili <gongzili456@gmail.com>
2018-04-06 16:15:19 +0800

    parse json

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ module.exports = function token(params, cb) {
   const store = params.store || new Store()
 
   store.load(key).then(function(token) {
+    token = JSON.parse(token)
     if (isValid(token)) {
       return cb(null, token)
     }
